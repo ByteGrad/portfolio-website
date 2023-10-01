84a5e304f7fa0cf217387476b307112cbbb15ae7
Markus Linke <markus.linke@linke.de>
2023-10-01 22:49:18 +0700

    customization

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
       <small className="mb-2 block text-xs">&copy; 2023 Markus. All rights reserved.</small>
-      <p className="text-xs">
+      {/* <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS, Framer Motion, React Email & Resend, Vercel
         hosting.
-      </p>
+      </p> */}
     </footer>
   );
 }
