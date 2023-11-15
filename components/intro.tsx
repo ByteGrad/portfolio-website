f63e9f856e727be8f7e76d634365e7c9a5d0ff78
jinhongee <pappies0827@gmail.com>
2023-11-14 19:45:49 -0500

    change profile picture

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,7 +31,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+              src="My_Photo.jpeg"
               alt="Ricardo portrait"
               width="192"
               height="192"
@@ -62,11 +62,8 @@ export default function Intro() {
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I'm Ricardo.</span> I'm a{" "}
-        <span className="font-bold">full-stack developer</span> with{" "}
-        <span className="font-bold">8 years</span> of experience. I enjoy
-        building <span className="italic">sites & apps</span>. My focus is{" "}
-        <span className="underline">React (Next.js)</span>.
+        <span className="font-bold">Hi, I'm Jinhong.</span> <br/>
+        I'm an undergrad at Columbia University studying CS! I enjoy Software Development and AI/ML Research. 
       </motion.h1>
 
       <motion.div
