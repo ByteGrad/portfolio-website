e6251dc67ff21aac6388a5fe0bcd5c3d75083703
Parth Parmar <84669955+Code-Parth@users.noreply.github.com>
2023-12-01 00:11:06 +0530

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -53,7 +53,7 @@ export default function Contact() {
         }}
       >
         <input
-          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-10 dark:focus:bg-opacity-15 text-white transition-all dark:outline-none"
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-10 dark:focus:bg-opacity-15 dark:text-white transition-all dark:outline-none"
           name="senderEmail"
           type="email"
           required
@@ -61,7 +61,7 @@ export default function Contact() {
           placeholder="Your email"
         />
         <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-10 dark:focus:bg-opacity-15 text-white transition-all dark:outline-none"
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-10 dark:focus:bg-opacity-15 dark:text-white transition-all dark:outline-none"
           name="message"
           placeholder="Your message"
           required
