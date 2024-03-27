c47a669de4e8dc9e81d83ab2b604b94a948f25d7
dvdgamer <davidnmponte@gmail.com>
2024-03-27 15:36:41 +0100

    Finished personalizing email

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "bytegrad@gmail.com",
+      to: "davidnmponte@gmail.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
